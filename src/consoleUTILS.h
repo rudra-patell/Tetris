2b17e6216aa70fe3bb3aad016cd122a377d30536
rudra-patell <202404028@daiict.ac.in>
2025-03-20 22:26:08 +0530

    Update consoleUTILS.h

--- src/consoleUTILS.h
+++ src/consoleUTILS.h
@@ -5,18 +5,18 @@
 
 // all the functions idea here where taken from sites like stackoverflow, geekforgeeks etc.
 
-inline void gotoxy(int x, int y){ //inline make the fuctions slightly faster its useful for small functions which are called more frequently
+void gotoxy(int x, int y){ //inline make the fuctions slightly faster its useful for small functions which are called more frequently
     COORD c;
     c.X = x;
     c.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
 
-inline void setColor(int color){
+void setColor(int color){
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
 
-inline void hideCursor(){
+void hideCursor(){
     CONSOLE_CURSOR_INFO cursor;
     cursor.dwSize = 100;
     cursor.bVisible = false;
