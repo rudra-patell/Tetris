07333b1dbf02d9f9bdcecc228e4e092f51f82c1d
rudra-patell <202404028@daiict.ac.in>
2025-03-26 21:45:29 +0530

    Update consoleUTILS.h

--- src/consoleUTILS.h
+++ src/consoleUTILS.h
@@ -5,7 +5,7 @@
 
 // all the functions idea here where taken from sites like stackoverflow, geekforgeeks etc.
 
-inline void gotoxy(int x, int y){ //inline make the fuctions slightly faster its useful for small functions which are called more frequently
+inline void gotoxy(int x, int y){ //inline fixes the multiple declarations error in linking stage
     COORD c;
     c.X = x;
     c.Y = y;
